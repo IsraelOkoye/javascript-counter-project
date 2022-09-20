6d845aacfb567559b15a0d0f60f68a8dd053cf3a
OkoyeIsrael <israelokoye2018@gmail.com>
2022-09-20 20:46:40 +0100

    javaScript counter project

---
+++ home.js
@@ -0,0 +1,17 @@
+
+let paragrapherror = document.getElementById("error")
+console.log(paragrapherror)
+
+function errormessage() {
+    paragrapherror.textContent = "Something went wrong, please try again latter"
+  }
+
+  
+
+
+
+
+
+ 
+
+
